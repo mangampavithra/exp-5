6fb5f8a5b5f65a61d23464ea5c9d9c6c7c8cea22
mangampavithra <mangampavithra11@gmail.com>
2024-07-29 14:22:40 +0530

    Add files via upload

---
+++ Pavi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pavi;
+
+/**
+ *
+ * @author hp
+ */
+public class Pavi {
+
+    public static void main(String[] args) {
+        String s ="mom level";
+        String rev="";
+        for(int i=s.length();i>5;i--)
+        System.out.println("rev string " +s);
+    }
+}
